626217e672ec8684d5e7e7e2693e2ffbcf214324
svub <>
2024-12-15 17:38:12 +0100

    formatting mostly

--- pages/contact/thanks.vue
+++ pages/contact/thanks.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="px-8 mx-auto mt-12 prose sm:px-6 md:px-4 lg:px-2 xl:px-0 xl:prose-xl lg:prose-lg md:prose-md">
+  <div
+    class="px-8 mx-auto mt-12 prose sm:px-6 md:px-4 lg:px-2 xl:px-0 xl:prose-xl lg:prose-lg md:prose-md"
+  >
     <h2>Thank you!</h2>
     <p>I'll be in touch with you soon!</p>
   </div>
